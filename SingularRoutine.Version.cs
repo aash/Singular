a2874cd22885bf520e0acf59e09e236a73086cae
aash <shpitalyov.a.alexey@gmail.com>
2015-09-01 00:26:12 +0600

    merge with Singular 4.4772

--- SingularRoutine.Version.cs
+++ SingularRoutine.Version.cs
@@ -33,8 +33,8 @@ using Styx.CommonBot.Routines;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("4.0.0.4725")]
-[assembly: AssemblyFileVersion("4.0.0.4725")]
+[assembly: AssemblyVersion("4.0.0.4772")]
+[assembly: AssemblyFileVersion("4.0.0.4772")]
 
 namespace Singular
 {
@@ -51,9 +51,9 @@ namespace Singular
     /// 
     /// Singular SVN Information
     /// -------------------------
-    /// Revision 4725
-    /// Date     2015/05/26 15:33:07
-    /// Range    4718:4725
+    /// Revision 4772
+    /// Date     2015/08/20 14:47:11
+    /// Range    4771:4772
     /// 
     /// </summary>
     public partial class SingularRoutine : CombatRoutine
@@ -65,11 +65,11 @@ namespace Singular
         // return Assembly.GetExecutingAssembly().GetName().Version;
         public static Version GetSingularVersion()
         {
-            return new Version("4.0.0.4725");
+            return new Version("4.0.0.4772");
         }
         public static string GetSingularBuildTime()
         {
-            return "2015/05/26 15:33:18";
+            return "2015/08/22 07:39:04";
         }
     }
 }
